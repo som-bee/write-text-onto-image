89c0638a8ea1fc79496de53ce6204fe03aba9aae
Somnath Bera <somnathbera1919@gmail.com>
2023-03-15 10:10:14 +0530

    added cover image file and modified Main.java file

---
+++ Main.java
@@ -0,0 +1,32 @@
+import java.io.File;
+import java.util.HashMap;
+
+public class Main{
+    public static void main(String[] args) {
+
+        HashMap<String, HashMap<String, Integer>> sectionCoordinates = new HashMap<String, HashMap<String, Integer>>();
+
+      
+      
+        File coverImg = new File("cover-img/cover-1024.png");
+
+
+
+
+
+        System.out.println("TESTING");
+        // BufferedImage img = null;
+        // try {
+        //     img = ImageIO.read();
+        // } catch (IOException e) {
+        //     e.printStackTrace();
+        // }
+        // Graphics g = img.getGraphics();
+        // g.setFont(g.getFont().deriveFont(30f));
+        // g.drawString("Hello", 100, 100);
+        // g.dispose();
+        // ImageIO.write(img, "png", new File("test/test.png"));
+        System.out.println("TESTING DONE...");
+
+    }
+}
