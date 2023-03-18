91a1c1170a9cfd3ce334cc592f9c820ef7be16af
Somnath Bera <somnathbera1919@gmail.com>
2023-03-18 22:43:48 +0530

    writing on image added and tested

--- ImgOperation.java
+++ ImgOperation.java
@@ -1,3 +1,51 @@
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.font.TextAttribute;
+
+import javax.imageio.ImageIO;
+
 public class ImgOperation {
-    
+
+    public static void writeTextOnImage(File coverImg,
+            HashMap<String, HashMap<String, HashMap<String, Integer>>> sectionCoordinates,
+            HashMap<String, String> chequeData, File coverOutImg) {
+
+        BufferedImage bufferedCoverImg = null;
+        try {
+            bufferedCoverImg = ImageIO.read(coverImg);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        Graphics g = bufferedCoverImg.getGraphics();
+        g.setFont(g.getFont().deriveFont(30f));
+        g.setColor(new Color(10, 10, 10));
+        Map<TextAttribute, Object> attributes = new HashMap<TextAttribute, Object>();
+        // adding space bettween letters as an text attribute
+        attributes.put(TextAttribute.TRACKING, 0.25);
+        g.setFont(g.getFont().deriveFont(attributes));
+
+        for (String key : chequeData.keySet()) {
+            g.drawString(chequeData.get(key),
+                    sectionCoordinates.get(key).get("start").get("x") + 5,
+                    sectionCoordinates.get(key).get("start").get("y") + 20);
+        }
+
+        g.dispose();
+        try {
+            ImageIO.write(bufferedCoverImg, "png", coverOutImg);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
 }
--- Main.java
+++ Main.java
@@ -1,34 +1,106 @@
 import java.io.File;
+import java.io.IOException;
+import java.lang.reflect.Array;
+import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
 
-public class Main{
+public class Main {
     public static void main(String[] args) {
 
-        HashMap<String, HashMap<String, HashMap<String, Integer>>> sectionCoordinates = new HashMap<String, HashMap<String, HashMap<String, Integer>>>();
-        HashMap<String, String > chequeData = new HashMap<String, String>();
+        Path sectionCoordinatesText = Path.of("cover-img/cheque-section-coordinates.txt");
+        Path chequeDataText = Path.of("input-text/cheque-data.txt");
 
-        
-      
-      
         File coverImg = new File("cover-img/hdfc.png");
 
+        File coverOutImg = new File("output-img/hdfc-cover.png");
 
-        
+        HashMap<String, HashMap<String, HashMap<String, Integer>>> sectionCoordinates = getSectionCoordinates(
+                sectionCoordinatesText);
 
+        HashMap<String, String> chequeData = getChequeDataFromText(chequeDataText);
 
-        System.out.println("TESTING");
-        // BufferedImage img = null;
-        // try {
-        //     img = ImageIO.read();
-        // } catch (IOException e) {
-        //     e.printStackTrace();
-        // }
-        // Graphics g = img.getGraphics();
-        // g.setFont(g.getFont().deriveFont(30f));
-        // g.drawString("Hello", 100, 100);
-        // g.dispose();
-        // ImageIO.write(img, "png", new File("test/test.png"));
-        System.out.println("TESTING DONE...");
+        System.out.println("WRITING TEXT ON IMAGE");
+        System.out.println(Arrays.asList(sectionCoordinates));
+        System.out.println(Arrays.asList(chequeData));
 
+        ImgOperation.writeTextOnImage(coverImg, sectionCoordinates, chequeData, coverOutImg);
+
+        System.out.println("WRITING TEXT ON IMAGE DONE...");
+
+    }
+
+    private static HashMap<String, String> getChequeDataFromText(Path chequeDataText) {
+
+        HashMap<String, String> chequeData = new HashMap<String, String>();
+
+        // Creating an object of Scanner class
+        Scanner sc;
+        try {
+            sc = new Scanner(chequeDataText);
+
+            while (sc.hasNext()) {
+                String line = sc.nextLine();
+                String[] strArr = line.split("-");
+                chequeData.put(strArr[0],strArr[1]);
+
+            }
+            sc.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return chequeData;
+    }
+
+    private static HashMap<String, HashMap<String, HashMap<String, Integer>>> getSectionCoordinates(
+            Path sectionCoordinatesText) {
+
+        HashMap<String, HashMap<String, HashMap<String, Integer>>> sectionCoordinates = new HashMap<String, HashMap<String, HashMap<String, Integer>>>();
+
+        // Creating an object of Scanner class
+        Scanner sc;
+        try {
+            sc = new Scanner(sectionCoordinatesText);
+
+            while (sc.hasNext()) {
+                String line = sc.nextLine();
+                String[] strArr = line.split(" ");
+                sectionCoordinates.put(strArr[0],
+                        new HashMap<String, HashMap<String, Integer>>() {
+                            {
+                                put("start",
+                                        new HashMap<String, Integer>() {
+                                            {
+                                                put("x", Integer.valueOf(strArr[1]));
+                                                put("y", Integer.valueOf(strArr[2]));
+
+                                            }
+                                        });
+                                put("end",
+                                        new HashMap<String, Integer>() {
+                                            {
+                                                put("x", Integer.valueOf(strArr[3]));
+                                                put("y", Integer.valueOf(strArr[4]));
+
+                                            }
+                                        });
+                            }
+
+                        });
+
+              
+            }
+            sc.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return sectionCoordinates;
     }
 }
